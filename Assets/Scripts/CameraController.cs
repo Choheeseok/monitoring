9402672b5b7a00afb7c48bad45f4e12d2cc0a300
Choheeseok <dswill038@gmail.com>
2021-01-15 02:55:42 +0900

    camera, navigation controll  -- bug: rotate too much first click navigation

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,98 +1,73 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
     public Transform target;
-    public Vector3 targetOffset;
-    public float distance = 5.0f;
-    public float maxDistance = 20;
-    public float minDistance = .6f;
-    public float xRotateSpeed = 1.0f;
-    public float yRotateSpeed = 1.0f;
-    public int yMinLimit = -90;
-    public int yMaxLimit = 90;
-    public int zoomRate = 40;
-    public float panSpeed = 0.3f;
-    public float zoomDampening = 0.5f;
- 
-    private float xDeg = 0.0f;
-    private float yDeg = 0.0f;
-    private float currentDistance;
-    private float desiredDistance;
-    private Quaternion desiredRotation;
-    private Quaternion rotation;
-    private Vector3 position;
- 
+    public Transform carUI;
+    public float xRotateSpeed;
+    public float yRotateSpeed;
+    public int zoomSpeed;
+    public float panSpeed;
+    public float maxWidth;
+    public float maxHeight;
+
+    public float xDeg = 0.0f;
+    public float yDeg = 0.0f;
+    private int yMinLimit = -90;
+    private int yMaxLimit = 90;
+    private NavController nav;
+    
+    void Awake()
+    {
+        transform.position = new Vector3(0,5,10);
+        transform.rotation = Quaternion.Euler(30,180,0);
+        carUI.eulerAngles = target.eulerAngles;
+    }
     void Start() { Init(); }
 
-    public void Init()
-    {
-        if (!target)
-        {
-            GameObject go = new GameObject("Cam Target");
-            go.transform.position = new Vector3(0, 0, 0);
-            target = go.transform;
-        }
- 
-        distance = Vector3.Distance(transform.position, target.position);
-        currentDistance = distance;
-        desiredDistance = distance;
-        
-        position = transform.position;
-        rotation = transform.rotation;
-        desiredRotation = transform.rotation;
- 
-        xDeg = Vector3.Angle(Vector3.right, transform.right );
-        yDeg = Vector3.Angle(Vector3.up, transform.up );
-    }
-    
     void Update()
     {
-        // Orbit
+        if (true == nav.isMoving)
+            return;
+        
         if (Input.GetMouseButton(1))
         //if (Input.GetMouseButton(2) && Input.GetKey(KeyCode.LeftControl))
         {
             xDeg += Input.GetAxis("Mouse X") * xRotateSpeed;
             yDeg -= Input.GetAxis("Mouse Y") * yRotateSpeed;
- 
-            ////////OrbitAngle
- 
-            //Clamp the vertical axis for the orbit
-            yDeg = ClampAngle(yDeg, yMinLimit, yMaxLimit);
-            // set camera rotation S
-            desiredRotation = Quaternion.Euler(yDeg, xDeg, 0);
 
-            rotation = desiredRotation;
-            transform.rotation = rotation;
-            // rotation = Quaternion.Lerp(currentRotation, desiredRotation, Time.deltaTime * zoomDampening);
-            //transform.rotation = rotation;
+            yDeg = ClampAngle(yDeg, yMinLimit, yMaxLimit);
+            transform.rotation = Quaternion.Euler(yDeg, xDeg, 0);
         }
-        // oif middle mouse is selected, we pan by way of transforming the target in screenspace
-        else if (Input.GetMouseButton(2))
+        if (Input.GetMouseButton(2))
         {
-            //grab the rotation of the camera so we can move in a psuedo local XY space
-            target.rotation = transform.rotation;
-            target.Translate(Vector3.right * -Input.GetAxis("Mouse X") * panSpeed);
-            target.Translate(transform.up * -Input.GetAxis("Mouse Y") * panSpeed, Space.World);
+            transform.Translate(Vector3.right * -Input.GetAxis("Mouse X") * panSpeed);
+            transform.Translate(Vector3.up * -Input.GetAxis("Mouse Y") * panSpeed);
         }
- 
-        ////////Orbit Position
         
-        // affect the desired Zoom distance if we roll the scrollwheel
-        desiredDistance -= Input.GetAxis("Mouse ScrollWheel") * Time.deltaTime * zoomRate * Mathf.Abs(desiredDistance);
-        //clamp the zoom min/max
-        desiredDistance = Mathf.Clamp(desiredDistance, minDistance, maxDistance);
-        // For smoothing of the zoom, lerp distance
-        //currentDistance = Mathf.Lerp(currentDistance, desiredDistance, Time.deltaTime * zoomDampening);
-        currentDistance = desiredDistance;
-        // calculate position based on the new currentDistance 
-        position = target.position - (rotation * Vector3.forward * currentDistance + targetOffset);
-        transform.position = position;
+        transform.Translate(transform.forward * Input.GetAxis("Mouse ScrollWheel") * Time.deltaTime * zoomSpeed,Space.World);
+
+        Vector3 pos = new Vector3(Screen.width * 0.9f, Screen.height * 0.9f, 10);
+        transform.position = new Vector3(
+            Mathf.Clamp(transform.position.x, -maxWidth, maxWidth),
+            Mathf.Clamp(transform.position.y, 0, maxHeight),
+            Mathf.Clamp(transform.position.z, -maxWidth, maxWidth));
+        carUI.position = Camera.main.ScreenToWorldPoint(pos);
+        carUI.eulerAngles = target.eulerAngles;
     }
  
+    private void Init()
+    {
+        nav = carUI.GetComponent<NavController>();
+
+        xDeg = Vector3.Angle(Vector3.right, transform.right );
+        yDeg = Vector3.Angle(Vector3.up, transform.up );
+    }
+    
     private static float ClampAngle(float angle, float min, float max)
     {
         if (angle < -360)
---
+++ Assets/Scripts/NavController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Xml.Serialization;
+using UnityEngine;
+using UnityEngine.WSA;
+
+public class NavController : MonoBehaviour
+{
+    public CameraController cameraController;
+    public Transform targetObject;
+    public Transform left;
+    public Transform right;
+    public Transform up;
+    public Transform down;
+    public Transform front;
+    public Transform back;
+
+
+    public bool isMoving = false;
+    private Transform desiredTransform;
+    private HashSet<Transform> directions;
+
+    private bool rotationCompleted = false;
+    private bool translateCompleted = false;
+    void Start()
+    {
+        isMoving = false;
+        directions = new HashSet<Transform>()
+        {
+            left, right, up, down, front, back
+        };
+        foreach (var t in directions)
+        {
+            t.LookAt(transform);
+        }
+    }
+    void Update()
+    {
+        if (false == isMoving)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+                if(Physics.Raycast(ray, out hit))
+                {
+                    if (directions.Contains(hit.transform))
+                    {
+                        isMoving = true;
+                        desiredTransform = hit.transform;
+                    }
+                }
+            }
+        }
+
+        if (true == isMoving)
+        {
+            StartCoroutine(InterpolateQuaternion(Camera.main.transform.rotation, desiredTransform.rotation, 0.5f));
+            Vector3 desiredPosition = targetObject.position - desiredTransform.forward * 10.0f;
+            StartCoroutine(InterpolateVector3(Camera.main.transform.position, desiredPosition, 0.5f));
+
+            if (translateCompleted && rotationCompleted)
+            {
+                isMoving = false;
+                translateCompleted = false;
+                rotationCompleted = false;
+            }
+        }
+    }
+
+    private IEnumerator InterpolateQuaternion(Quaternion src, Quaternion dest, float time)
+    {
+        Quaternion source = new Quaternion(src.x,src.y,src.z,src.w);
+        float startTime = Time.time;
+        while (Time.time < startTime + time)
+        {
+            Camera.main.transform.rotation = Quaternion.Lerp(source, dest, (Time.time-startTime)/time);
+            cameraController.xDeg = Camera.main.transform.rotation.eulerAngles.y;
+            cameraController.yDeg = Camera.main.transform.rotation.eulerAngles.x;
+            yield return null;
+        }
+        Camera.main.transform.rotation = dest;
+        cameraController.xDeg = Camera.main.transform.rotation.eulerAngles.y;
+        cameraController.yDeg = Camera.main.transform.rotation.eulerAngles.x;
+        rotationCompleted = true;
+    }
+    
+    private IEnumerator InterpolateVector3(Vector3 src, Vector3 dest, float time)
+    {
+        Vector3 source = new Vector3(src.x,src.y,src.z);
+        float startTime = Time.time;
+        while (Time.time < startTime + time)
+        {
+            Camera.main.transform.position = Vector3.Lerp(source, dest, (Time.time-startTime)/time);
+            yield return null;
+        }
+        Camera.main.transform.position = dest;
+        translateCompleted = true;
+    }
+}
