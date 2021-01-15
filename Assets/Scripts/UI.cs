c1d5ac05ed6dedeabf12d288d8f07e98f6e96c99
Choheeseok <dswill038@gmail.com>
2021-01-15 23:55:26 +0900

    navigation

---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour
+{
+    public Button button;
+    public Transform navCar;
+
+    private bool activated = true;
+    
+    public void OnClick()
+    {
+        activated = !activated;
+        if (false == activated)
+        {
+            button.GetComponent<Image>().color = new Color(0, 0, 0, 1.0f);
+        }
+        else
+        {
+            button.GetComponent<Image>().color = new Color(0, 0, 0, 0.4f);
+        }
+        navCar.gameObject.SetActive(activated);
+    }
+}
